8da248908f88814a7da8e1bfbd67bc97f346e508
predeep kumar singroul <prasingroul4899@gmail.com>
2024-01-04 16:22:15 +0530

    Button css

--- ironhaven/src/App.js
+++ ironhaven/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import AllRoutes from './pages/Routes';
+import About from './pages/About/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Header/>
+        <AllRoutes/>
     </div>
   );
 }
